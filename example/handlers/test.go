bf291757c609c38059b7e8352977b45969e63269
Byron <byron@live.com>
2020-07-02 11:35:04 +0800

    complete: header add / set

--- example/handlers/test.go
+++ example/handlers/test.go
@@ -4,17 +4,23 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/byronzr/micro/helper"
 )
 
 type POST struct{}
 type OPTIONS struct{}
 
 func (POST) ReportThisweek(r *http.Request) (response []byte, err error) {
-	r.Response = &http.Response{}
-	r.Response.Header = http.Header{}
-	r.Response.Header.Add("cool", "damn")
+
+	// like http.Header.Add
+	helper.HeaderAdd(r, "foo", "bar")
 	fmt.Printf("%#v\n", r)
-	fmt.Println(len(r.Response.Header))
+
+	// like http.Header.Set
+	helper.HeaderSet(r, "foo", "bar1")
+	fmt.Printf("%#v\n", r)
+
 	msg := "POST.ReprotThisweek."
 	return json.Marshal(msg)
 }
